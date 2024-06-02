8b665d2126edd1477080841661584b09b09ee229
Hozef Selim <hothefa180@gmail.com>
2024-06-02 09:09:43 +0300

    Edit classes

---
+++ Kanban Tracker/Classes/Project.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kanban_Tracker.Classes
+{
+    public class Project
+    {   
+        public int ProjectID { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectDescription { get; set; }
+        public string Status { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+
+        public Project(int projectID, string projectName, string projectDescription, string status, DateTime? startDate, DateTime? endDate)
+        {
+            ProjectID = projectID;
+            ProjectName = projectName;
+            ProjectDescription = projectDescription;
+            Status = status;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
