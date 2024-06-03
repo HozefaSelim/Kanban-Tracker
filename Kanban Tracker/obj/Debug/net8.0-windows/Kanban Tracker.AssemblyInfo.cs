18043f74578f75e7d60908c10eb96a9d7eb2c2fd
Hozef Selim <hothefa180@gmail.com>
2024-06-03 08:32:24 +0300

    handle the olustur button and board

--- Kanban Tracker/obj/Debug/net8.0-windows/Kanban Tracker.AssemblyInfo.cs
+++ Kanban Tracker/obj/Debug/net8.0-windows/Kanban Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kanban Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7109f596228cc5f8fdd3300be409fde400315e36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4fbc0f7fe33df94cdf8fc3f8527d182b3ec11e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kanban Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kanban Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
